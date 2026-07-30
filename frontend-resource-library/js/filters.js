19730a26e5b3d563adcc41db05aa50d714c2dbb4
Mia Carrillo <mi245047@ucf.edu>
2026-07-30 12:52:21 -0400

    Initial commit: Front-End Resource Library app

--- frontend-resource-library/js/filters.js
+++ frontend-resource-library/js/filters.js
@@ -1,12 +1,23 @@
 export function searchResources(resources, query) {
   const lowerQuery = query.toLowerCase();
 
-  return resources.filter(function (resource) 
-  {
+  const filteredResources = resources.filter(function (resource) {
     const titleMatch = resource.title.toLowerCase().includes(lowerQuery);
     const topicMatch = resource.topic.toLowerCase().includes(lowerQuery);
     const descriptionMatch = resource.description.toLowerCase().includes(lowerQuery);
-    
+
     return titleMatch || topicMatch || descriptionMatch;
   });
+
+  if (filteredResources.length === 0) {
+    return [
+      {
+        title: 'No results found',
+        topic: '',
+        description: ''
+      }
+    ];
+  }
+
+  return filteredResources;
 }
